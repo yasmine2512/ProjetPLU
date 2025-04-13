3a08ad76f6ed74a020ba65cb41ed400a7bc3aaf6
yasmine2512 <155321893+yasmine2512@users.noreply.github.com>
2025-04-13 18:55:43 +0100

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+namespace platformApp.Models;
+
+public class User
+{
+    public int UserID { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+    public string Major { get; set; }
+    public string University { get; set; }
+    public string Specialty { get; set; }
+}
