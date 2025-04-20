04453f860b30d769c4e68e4b2ff18841f6537c40
yasmine2512 <155321893+yasmine2512@users.noreply.github.com>
2025-04-20 13:59:27 +0100

    Create Favorite.cs

---
+++ Models/Favorite.cs
@@ -0,0 +1,13 @@
+using platformApp.Models;
+using Memoireclass.Models;
+
+namespace Favorite.Models{
+public class Favorite
+{
+    public int UserID { get; set; }
+    public int MemoireID { get; set; }
+
+    // Navigation properties
+    public User User { get; set; }
+    public Memoire Memoire { get; set; }
+}}
