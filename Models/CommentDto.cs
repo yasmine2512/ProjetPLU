c24a9bbe7f5615bc21e82c96f73dc46f93b3791e
yasmine2512 <155321893+yasmine2512@users.noreply.github.com>
2025-04-20 13:58:56 +0100

    Create CommentDto.cs

---
+++ Models/CommentDto.cs
@@ -0,0 +1,12 @@
+namespace CommentDto.Models
+{
+public class CommentDto
+{
+    public int CommentID { get; set; }
+    public int UserID { get; set; }
+    public string Text { get; set; }
+    public DateTime Date { get; set; }
+
+    public string FullName { get; set; }
+    public string PicturePath { get; set; }
+}}
