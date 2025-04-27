add8cab32151f45563c37663c3b34f9c04ba25ec
Wafaafra <155725928+Wafaafra@users.noreply.github.com>
2025-04-27 18:51:58 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using platformApp;
+using MyBlazorFrontend;
+using MyBlazorFrontend.Models;
+using MyBlazorFrontend.Services;
 
 
 
@@ -8,13 +10,16 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Register the HTTP client
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("http://localhost:5110/")
+    BaseAddress = new Uri("http://localhost:5110/") // Your backend URL
 });
+
+// Register other services
+builder.Services.AddSingleton<SummarizeService>(); // Add SummarizerService
+
+// Register UserState
 builder.Services.AddScoped<UserState>();
 
-
-
-
- await builder.Build().RunAsync();
+await builder.Build().RunAsync();
