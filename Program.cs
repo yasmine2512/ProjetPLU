ba8a2d1a0ff278cfca438b26cd25cd19eeee3510
yasmine2512 <155321893+yasmine2512@users.noreply.github.com>
2025-05-23 16:38:19 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyBlazorFrontend;
 using MyBlazorFrontend.Models;
-using MyBlazorFrontend.Services;
+
 
 
 
@@ -16,8 +16,7 @@ builder.Services.AddScoped(sp => new HttpClient
     BaseAddress = new Uri("http://localhost:5110/") // Your backend URL
 });
 
-// Register other services
-builder.Services.AddSingleton<SummarizeService>(); // Add SummarizerService
+
 
 // Register UserState
 builder.Services.AddScoped<UserState>();
